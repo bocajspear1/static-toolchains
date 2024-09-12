959e5b3abba35e266ffd12babb818bf9c05a3944
Jacob Hartman <junyunharold1@gmail.com>
2024-09-11 22:10:53 -0400

    Fixes for fixes

--- build_musl.sh
+++ build_musl.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 BUILD_ARCH=$1
 CPU_COUNT="$(($(nproc --all)-1))"
 
 mkdir -p ./output
-docker buildx build --build-arg BUILD_ARCH=${BUILD_ARCH} CPU_COUNT=${CPU_COUNT} .
+docker buildx build --build-arg BUILD_ARCH=${BUILD_ARCH} --build-arg CPU_COUNT=${CPU_COUNT} .
 
 pushd output
 tar -zcf ../${BUILD_ARCH}-musl-toolchain.tar.gz *
